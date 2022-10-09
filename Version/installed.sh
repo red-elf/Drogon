dcdd0a2ec18730935e9c0aeb09d8a4244f1bb90f
Milos Vasic <i@mvasic.ru>
2022-10-09 13:55:41 +0300

    Version set.

---
+++ Version/installed.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# TODO:
+echo "TODO <<<<<<"
+exit 1
+
+#HERE="$(pwd)"
+#VERSION="Version: "
+#VERSION_SCRIPT="$HERE/Version/version.sh"
+#
+#if ! test -e "$VERSION_SCRIPT"; then
+#
+#  echo "ERROR: The version script was not found at path: '$VERSION_SCRIPT'"
+#  exit 1
+#fi
+#
+## shellcheck disable=SC1090
+#. "$VERSION_SCRIPT"
+#
+#PC_FILE="/usr/local/lib/pkgconfig/$VERSIONABLE_NAME.pc"
+#if ! test -e "$PC_FILE"; then
+#
+#  PC_FILE="/usr/local/lib64/pkgconfig/$VERSIONABLE_NAME.pc"
+#fi
+#
+## shellcheck disable=SC2002
+#RAW="$(test -e "$PC_FILE" && cat "$PC_FILE" | grep "$VERSION")" && \
+#  echo "${RAW/$VERSION/}" | xargs
---
+++ Version/version.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export VERSIONABLE_NAME="drogon"
+export VERSIONABLE_VERSION_PRIMARY="1"
+export VERSIONABLE_VERSION_SECONDARY="8"
+export VERSIONABLE_VERSION_PATCH="1"
+export VERSIONABLE_HOMEPAGE="https://github.com/drogonframework/drogon"
+export VERSIONABLE_DESCRIPTION="A C++14/17/20 based HTTP web application framework running on Linux/macOS/Unix/Windows"
